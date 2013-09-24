ace9fd496ff9b24e5537b957097c680cbc3976a3
Ben Curren <ben@thecurrens.com>
2013-09-23 17:02:32 -0700

    added sig for FindBridge, need to add some more functionaality to udpn to finish

--- bridge.go
+++ bridge.go
@@ -1,5 +1,10 @@
 package hue
 
+import (
+	"github.com/bcurren/go-udpn"
+	"time"
+)
+
 type Bridge struct {
 	client *client
 }
@@ -8,6 +13,16 @@ func NewBridge(addr string) *Bridge {
 	client := NewHttpClient(addr)
 	return &Bridge{client: client}
 }
+
+// TODO: Need to add some functionality to udpn library
+// func FindBridges() ([]*Bridge, error) {
+// 	_, err := udpn.Search("upnp:rootdevice", 3*time.Second)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	return nil, nil
+// }
 
 func (b *Bridge) CreateUser(deviceType, username string) (*User, error) {
 	url := "/api"
